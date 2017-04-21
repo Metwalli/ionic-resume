0249ed1bb14dd4a32a40e8a624cdcdb7ef337826
Metwalli <Metwalli.msn@gmail.com>
2017-04-21 21:07:58 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { ModalController, Platform, NavParams, ViewController } from 'ionic-angular';
+//import { AuthComponent } from './auth/auth.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','./home/styles.css','./home/font_style.css','./home/style_rtl.css','./home/dialog.css']
 })
 export class AppComponent {
   title = 'app works!';
+  authType: string = 'login';  
+  @ViewChild('editUserDialog') dialog: any;
+  constructor(
+    public modalCtrl: ModalController
+    ) { }  
+  showDialog(at: string){
+    this.authType= at;
+    let modal = this.modalCtrl.create(ModalContentPage,{authType: this.authType});
+    modal.present();
+  }
+}
+
+@Component({
+  template: `
+    <app-auth [authType]="authType"></app-auth>
+  `
+})
+export class ModalContentPage {
+  authType: string = 'login';
+  constructor(
+    public platform: Platform,
+    public params: NavParams,
+    public viewCtrl: ViewController
+  ) {
+    this.authType = this.params.get('authType');
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,37 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MdlModule } from 'angular2-mdl';
+import { ResumeVersionModule } from './resume-version/resume-version.module';
+import { AppComponent, ModalContentPage } from './app.component';
+import { CoreModule } from './core/core.module';
+import { ExpansionPanelModule } from './expansion-panel/expansion-panel.module';
+import { AppRoutingModule } from './app.routing.module';
+import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalContentPage,
+    HomeComponent
   ],
+  entryComponents:[ ModalContentPage],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdlModule,
+    IonicModule.forRoot(AppComponent),
+    CoreModule, 
+    AuthModule,
+    ExpansionPanelModule,
+    ResumeVersionModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AppRoutingModule],
+  //schemas:[ CUSTOM_ELEMENTS_SCHEMA ],
+  bootstrap: [IonicApp]
 })
 export class AppModule { }
